fd753797589afe3635af6543df9ecd5b8fb22627
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-07-17 07:49:47 -0700

    add component dir, add TopStories comp, remove boilerplate.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TopStories from './TopStories';
+
+function App() {
+  return (
+    <React.Fragment>
+      <TopStories />
+    </React.Fragment>
+  );
+}
+
+export default App;
